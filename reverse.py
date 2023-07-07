ebfc57a00d99a7306443e8ac99d69dbfa35e84de
ankitgautam474 <127035981+ankitgautam474@users.noreply.github.com>
2023-07-07 17:44:24 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,9 @@
+word = input("Enter a word: ")
+
+reversed_word = ''
+
+# iterate over the characters in reverse order
+for i in range(len(word) - 1, -1, -1):
+    reversed_word += word[i]
+
+print("Reversed word:", reversed_word)
